846ad395abadc5f19f98fc92f49cc7a03b7d3433
HoshinAkoko <nagiawa@foxmail.com>
2023-04-28 23:58:09 +0800

    First commit!

---
+++ KCPS/KCPSPlugin.cs
@@ -0,0 +1,22 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Interface.Windowing;
+
+namespace KCPS
+{
+    public sealed class KCPSPlugin : IDalamudPlugin
+    {
+        public string Name => "KCPS Plugin";
+        
+        
+        
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
